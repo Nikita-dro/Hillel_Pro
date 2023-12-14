a8c9e7d5a84df7f054b21f8b9a90434925f85ee3
mykyta <drobotda598@gmail.com>
2023-12-14 21:03:38 +0000

    Changed generator

--- Hw24/task_2.py
+++ Hw24/task_2.py
@@ -1,13 +1,19 @@
-from faker import Faker
+import nltk
+from nltk.corpus import words
+import random
+
+nltk.download('words')
 
 
-def generate_random_words(num_words: int):
+def generate_realistic_words(num_words: int):
     if type(num_words) != int or num_words > 10000:
         raise ValueError("The number of words must be less or equal 10,000, type(num_words) must be int")
-    fake = Faker()
-    while num_words > 0:
-        yield fake.unique.word()
-        num_words -= 1
+    word_list = list(words.words())
+    random.shuffle(word_list)
+    unique_words = set(word_list)
+    for word in list(unique_words)[:num_words]:
+        yield word
 
 
-print(list(generate_random_words(10)))
+print(len(list(generate_realistic_words(9999))))
+print(len(set(generate_realistic_words(9999))))
